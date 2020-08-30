c7399d3a6cef8e052ae1224fe2c27cf9bc8d528c
Will Harris <will.t.harris@protonmail.com>
2020-08-30 12:15:29 -0700

    add macros solutions

--- exercises/macros/macros4.rs
+++ exercises/macros/macros4.rs
@@ -1,8 +1,6 @@
 // macros4.rs
 // Make me compile! Execute `rustlings hint macros4` for hints :)
 
-// I AM NOT DONE
-
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
@@ -14,5 +12,5 @@ macro_rules! my_macro {
 
 fn main() {
     my_macro!();
-    my_macro!(7777);
+    my_macro!("Boop");
 }
